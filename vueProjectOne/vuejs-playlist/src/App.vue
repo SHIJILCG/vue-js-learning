0ec410b80ab9635cd6cafe5e7f05938514a03b5b
Shijil <shijilcg@gmail.com>
2024-11-01 10:26:46 +0530

    first commit

---
+++ vueProjectOne/vuejs-playlist/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Shijil App",
+    };
+  },
+};
+</script>
+
+<style></style>
